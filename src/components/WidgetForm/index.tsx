5eae4a98c44545a009c314abf0639487a6a02e43
Gilberto <gilbertocarvalho1997@gmil.com>
2022-05-08 18:19:24 -0300

    first commit

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,68 @@
+import { CloseButton } from "../CloseButton";
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "imagem de uma lampada",
+    },
+  },
+  OUTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "imagem de nuvem de pensamento",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+      setFeedbackSent(false)
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              onFeedbackRestartRequested={handleRestartFeedback}
+              feedbackType={feedbackType}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs text-neutral-400 mt-2">
+        Feito na NLW Return
+      </footer>
+    </div>
+  );
+}
